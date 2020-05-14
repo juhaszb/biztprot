9a64d9b6ce4a08a95493ecfd2fc60247e2499430
Juhasz Balazs <juhaszb.98.01@gmail.com>
2020-05-14 19:35:42 +0200

    server modifications

---
+++ server/messagetype.h
@@ -0,0 +1,17 @@
+#ifndef __message_type__
+#define __message_type__
+
+#define MKD '0'
+#define RMD '1'
+#define GWD '2'
+#define CWD '3'
+#define LST '4'
+#define UPL '5'
+#define DNL '6'
+#define RMF '7'
+#define EXIT '8'
+#define REGISTER '9'
+#define LOGIN 'a'
+
+
+#endif
